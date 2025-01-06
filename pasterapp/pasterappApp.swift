1b54623a1e843635b07e7ff6897f24e947847f32
Ario Moniri <92126657+ArioMoniri@users.noreply.github.com>
2025-01-06 13:38:53 +0300

    Initial Commit

---
+++ pasterapp/pasterappApp.swift
@@ -0,0 +1,17 @@
+//
+//  pasterappApp.swift
+//  pasterapp
+//
+//  Created by Ariorad Moniri on 06/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct pasterappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pasterappTests/pasterappTests.swift
@@ -0,0 +1,17 @@
+//
+//  pasterappTests.swift
+//  pasterappTests
+//
+//  Created by Ariorad Moniri on 06/01/2025.
+//
+
+import Testing
+@testable import pasterapp
+
+struct pasterappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
