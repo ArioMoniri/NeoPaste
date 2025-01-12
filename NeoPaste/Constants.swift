55a4b1014cdbea7915c523efc1659cdfe8df259b
Ario Moniri <92126657+ArioMoniri@users.noreply.github.com>
2025-01-13 00:33:18 +0300

    added usefinderwindow

--- NeoPaste/Constants.swift
+++ NeoPaste/Constants.swift
@@ -1,19 +1,3 @@
-//
-// Copyright 2025 Ariorad Moniri
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-
 import Foundation
 import UniformTypeIdentifiers
 import SwiftUI
@@ -48,6 +32,7 @@ enum UserDefaultsKeys {
     static let darkModeEnabled = "darkModeEnabled"
     static let autoStartEnabled = "autoStartEnabled"
     static let compressFiles = "compressFiles"
+    static let useFinderWindow = "useFinderWindow"
 }
 
 // MARK: - Notification Names
